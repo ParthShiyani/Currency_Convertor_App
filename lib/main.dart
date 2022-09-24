48e8b36f3c60eb7c657d5e27ee786181410e32df
Parth Shiyani <parthshiyani143@gmail.com>
2022-09-24 18:05:20 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:currency_convertor_app/screens/homepage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+bool isIos = false;
+
+void main() {
+  runApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return (isIos == false)
+        ? MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: const HomePage(),
+          )
+        : CupertinoApp(
+            debugShowCheckedModeBanner: false,
+            home: const HomePage(),
+          );
+  }
+}
---
+++ lib/models/currency.dart
@@ -0,0 +1,14 @@
+class Currency {
+  final Map<String, double> allRates;
+
+  Currency({
+    required this.allRates,
+  });
+
+  factory Currency.allCurrenciesFromJson({required Map<String, dynamic> json}) {
+    return Currency(
+      allRates: Map.from(json['rates'])
+          .map((key, value) => MapEntry<String, double>(key, value.toDouble())),
+    );
+  }
+}
